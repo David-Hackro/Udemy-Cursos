7de40fab8606ec49d80133433f54f6bf2a41bf23
Macbook Pro <macbookpro@tutorials.local>
2018-04-10 20:36:13 -0500

    add class Retrofit

---
+++ Retrofit/Retrofit2/app/src/main/java/hackro/tutorials/com/retrofit/MainActivity.kt
@@ -0,0 +1,14 @@
+package hackro.tutorials.com.retrofit
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        BuildConfig.BASE_URL //URL
+    }
+}
