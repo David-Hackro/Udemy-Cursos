ead7a71abcf647314c0c0d328384a9e058084828
Macbook Pro <macbookpro@tutorials.local>
2018-04-10 21:36:36 -0500

    add Class_O5

--- Retrofit/Clase_05/app/src/main/java/hackro/tutorials/com/retrofit/HackroService.kt
+++ Retrofit/Clase_05/app/src/main/java/hackro/tutorials/com/retrofit/HackroService.kt
@@ -1,42 +1,56 @@
 package hackro.tutorials.com.retrofit
 
-import retrofit2.http.DELETE
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.PUT
-
-/** Por practica, en Retrofit 2 los endpoint no deben comenzar con / **/
+import retrofit2.http.*
+/**Interface que tendra los metodos de consumo para el servicio de la aplicacion**/
 interface HackroService{
 
     /**
-     * Métodos de petición HTTP
-     *  @GET        Utilizado mas comunmente para recuperar/leer
-     *  @POST       Ocupado comunmente en formularios
-     *  @DELETE     Simplemente elimina un resource identificado en la URI
-     *  @PUT        Utilizado normalmente para actualizar contenidos
-     *
-     * Mas informacion: https://stackoverflow.com/a/47831350/3741698
-     **/
+     * La anotacion Query nos permite enviar parametros atraves de la url
+     *      @Query("llave")      + [nombre de la variable]   + [Tipo de dato]
+     *      @Query("id_user")    + [id]                      + [String]
+     */
+
 
     /**
-     * Recuerda que entre los paréntesis indicamos el endpoint que complementa a la url base
-     *  URL_BASE +  BuildConfig.ENDPOINT_USER
+     * Podemos enviar de forma "hardcodeada" los valores
+     */
+    @GET(BuildConfig.ENDPOINT_USER+"?id_user=valor") fun getUser() : List<String>
+
+    /**
+     * Lo ideal es utilizar la anotacion Query
+     * Ejemplo de la url:   /user?id_user=valor
      * **/
-
-
-    @GET(BuildConfig.ENDPOINT_USER) fun getUser() : List<String>
-
-
-    @POST(BuildConfig.ENDPOINT_USER) fun postUser() : String
-
-
-    @PUT(BuildConfig.ENDPOINT_USER) fun putUser() : Int
-
-
-    @DELETE(BuildConfig.ENDPOINT_USER) fun deleteUser() : Int
+    @GET(BuildConfig.ENDPOINT_USER) fun getUser(@Query("id_user") id : String) : List<String>
 
 
 
+    /**
+    *Multiples parametros
+     * Enviamos parametros por separado :
+     *
+     * id_user  = 6464
+     * user     = hackro
+     * name     = david
+     *
+     * Ejemplo de la URL; /user?id_user=6464&user=hackro&name=david
+     **/
+    @GET(BuildConfig.ENDPOINT_USER) fun getUser(@Query("id_user") id : String,
+                                                @Query("user") user : String,
+                                                @Query("name") name : String) : List<String>
+
+
+
+    /**
+     * Multiples parametros con el mismo nombre
+     * Enviamos una lista con los valores :
+     *
+     * id_user  = 1
+     * id_user  = 233
+     * id_user  = 363
+     *
+     * Ejemplo de la URL; /user?id_user=1&id_user=233&id_user=363&.......
+     **/
+    @GET(BuildConfig.ENDPOINT_USER) fun getUser(@Query("id_user") list :List<String>) : List<String>
 
 
 
